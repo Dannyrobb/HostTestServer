a758d30056191695ccde4ebd0bbfe9f7a8b20a2e
Dannyrobb <107500349+Dannyrobb@users.noreply.github.com>
2023-03-27 18:09:46 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 3001;
+app.use(cors());
+app.use(express.json());
+require("dotenv").config();
+const db = require("knex")({
+  client: "pg",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+  },
+});
+app.get("/", async (req, res) => {
+  try {
+    const info = await db("users").select();
+    res.json(info);
+  } catch (e) {
+    res.json(e);
+  }
+});
+app.listen(process.env.PORT, () => console.log(`Listening on port ${process.env.PORT}...`));
